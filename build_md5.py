cc5f7204f4eab6d27eb93825ec88106a8450533b
U-pearl\ravio <sooriravindra@gmail.com>
2016-06-25 14:31:12 +0530

    Added support for md5

---
+++ build_md5.py
@@ -0,0 +1,25 @@
+import json
+import hashlib
+
+def calc_md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+index_file = open('index.json','r')
+index_json_raw = index_file.read()
+index_file.close()
+
+index_json = json.loads(index_json_raw)
+base_path = index_json['basePath']
+configs = index_json['configs']
+
+for config in configs:
+    md5 = calc_md5(base_path+config['file'])
+    config.update({'md5':md5})
+x = json.dumps(index_json,indent=2)    
+index_file = open('index.json','w')
+index_file.write(x)
+index_file.close()
